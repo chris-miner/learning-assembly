01f8061ea59bc9c0bdaf77912952d7d26cea7e7f
chris-miner <94078897+chris-miner@users.noreply.github.com>
2022-09-04 13:20:34 -0600

    put data in with text in single file since that's the way osx likes it by default.

--- chapter07/tallest.s
+++ chapter07/tallest.s
@@ -1,5 +1,37 @@
-.text
 .globl start
+.data
+# In the book this is all in another file called persondata.s
+# You can do that under linux, but not under macos.  The constants
+# are misunderstood to be regular variables, rather than constants.
+# So the assembler treats HEIGHT_OFFSET as an address (0x10) rather than
+# a value (16).  So we have to put it all in one file.
+# person record and fields
+.globl PERSON_RECORD_SIZE
+.equ PERSON_RECORD_SIZE, 0x28
+
+.globl WEIGHT_OFFSET, HAIR_OFFSET, HEIGHT_OFFSET, AGE_OFFSET
+.equ WEIGHT_OFFSET, 0x00
+.equ SHOE_SIZE_OFFSET, 0x08
+.equ HAIR_OFFSET, 0x10
+.equ HEIGHT_OFFSET, 0x18
+.equ AGE_OFFSET, 0x20
+
+# array of person records
+.globl people, numpeople
+people:
+    .quad 200, 10, 2, 74, 20
+    .quad 280, 12, 2, 72, 44 # author
+    .quad 225, 13, 3, 73, 54 # me
+    .quad 150, 8, 1, 68, 30
+    .quad 250, 14, 3, 75, 24
+    .quad 250, 10, 2, 70, 11
+    .quad 180, 11, 5, 69, 65
+endpeople:
+
+numpeople:
+    .quad (endpeople - people) / PERSON_RECORD_SIZE 
+
+.text
 start:
     # initialize registers
     # base address of people array in %rbx
@@ -29,41 +61,10 @@ mainloop:
     movq %rax, %rdi
 
 endloop:
-    addq $32, %rbx
+    addq $PERSON_RECORD_SIZE, %rbx
     loop mainloop
     
 finish:
     movq $0x2000001, %rax
     syscall
 
-.data
-
-# In the book this is all in another file called persondata.s
-# You can do that under linux, but not under macos.  The constants
-# are misunderstood to be regular variables, rather than constants.
-# So the assembler treats HEIGHT_OFFSET as an address (0x10) rather than
-# a value (16).  So we have to put it all in one file.
-# person record and fields
-.globl PERSON_RECORD_SIZE
-.equ PERSON_RECORD_SIZE, 32
-
-.globl WEIGHT_OFFSET, HAIR_OFFSET, HEIGHT_OFFSET, AGE_OFFSET
-.equ WEIGHT_OFFSET, 0
-.equ HAIR_OFFSET, 8
-.equ HEIGHT_OFFSET, 16
-.equ AGE_OFFSET, 24
-
-# array of person records
-.globl people, numpeople
-people:
-    .quad 200, 2, 74, 20
-    .quad 280, 2, 72, 44 # author
-    .quad 225, 3, 73, 54 # me
-    .quad 150, 1, 68, 30
-    .quad 250, 3, 75, 24
-    .quad 250, 2, 70, 11
-    .quad 180, 5, 69, 65
-endpeople:
-
-numpeople:
-    .quad (endpeople - people) / PERSON_RECORD_SIZE 
