2ccce12130a84fabf2cdfbb0f0ed6670b3a5f930
chris-miner <94078897+chris-miner@users.noreply.github.com>
2022-09-09 11:36:27 -0600

    stash/restore of rcx via r12. according to abi, 12-15 are callee preserved.  So those are safe.

--- chapter10/tenfold.s
+++ chapter10/tenfold.s
@@ -1,4 +1,4 @@
-# Print out a string 10 times.  I could have used r10 directly for
+# Print out a string 10 times.  I could have used r12 directly for
 # the loop counter, decremented it myself in loopcontrol, manually
 # did the conditional loop.  However, I wanted to highlight the fact
 # that rcx (and r11) are clobbered by any syscall.
@@ -20,9 +20,9 @@ mainloop:
     mov $1, %rdi
     lea msg(%rip), %rsi
     mov $len, %rdx
-    mov %rcx, %r10         # preserve rcx which is clobbered by syscall
+    mov %rcx, %r12         # preserve rcx which is clobbered by syscall
     syscall
-    mov %r10, %rcx         # restore rcx which was clobbered by syscall
+    mov %r12, %rcx         # restore rcx which was clobbered by syscall
     jc err
     loop mainloop
 
