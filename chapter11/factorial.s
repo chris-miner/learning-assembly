3a521a565fb0f589099c2c2644bf7c3d607a0cd8
chris-miner <94078897+chris-miner@users.noreply.github.com>
2022-09-09 16:01:37 -0600

    using the stack to keep track of numbers to multiply.

--- chapter11/factorial.s
+++ chapter11/factorial.s
@@ -1,6 +1,8 @@
-# compute the factorial of a number
+# compute the factorial of a number using the stack.  First push each 
+# value we need to mulitply by onto the stack.  Then pop the stack and 
+# mulitply until we get to zero.
 .text
-.equ NUMBER, 5
+.equ NUMBER, 4
 .equ INITIAL, 1
 
 .globl start
@@ -8,10 +10,24 @@ start:
     mov $INITIAL, %ax
     mov $NUMBER, %cx
 
-mainloop:
-    mul %cx
-    loop mainloop
+    # sentinel value to stop the multiplication loop
+    push $0
 
+# push 
+pushloop:
+    push %cx
+    loop pushloop
+
+# ax already contains the initial value of 1
+mainloop:
+    pop %bx
+    cmp $0, %bx
+    je end
+    mul %bx
+loopcontrol:
+    jmp mainloop
+
+end:
     mov %ax, %di
     mov $0x2000001, %rax
     syscall
