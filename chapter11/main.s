69622778c46d54c61efa3b2e63689ec446418b07
chris-miner <94078897+chris-miner@users.noreply.github.com>
2022-09-13 12:24:40 -0600

    reverted main to style for static library that I've been using.

--- chapter11/main.s
+++ chapter11/main.s
@@ -1,23 +1,20 @@
 # This program calculates the result raising a base to a power using a function.
 # the power function is defined as power(base, power) and implemented in power.s
-.globl _main
+.globl start
 .text
 
 # define some constants
 .equ BASE, 3
 .equ POWER, 4
 
-_main:
-    # no local variables
-    enter $0, $0
-    
+start:    
     mov $BASE, %rdi
     mov $POWER, %rsi
     call power
     
     mov %rax, %rdi
-    leave
-    ret
+    call exit
+    
 
 # takes one parameter and uses that as the exit code.
 exit:
