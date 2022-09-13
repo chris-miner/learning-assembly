d95b3bef3af2fae0e49c2b8824a072b7f4fc1c80
chris-miner <94078897+chris-miner@users.noreply.github.com>
2022-09-13 12:23:59 -0600

    update comments, made use of a gratuitous local variable to demonstrate the idea.

--- chapter11/power.s
+++ chapter11/power.s
@@ -5,18 +5,27 @@
 .text
 
 power:
-    # power takes two quad word unsigned arguments (RDI, RSI) and returns
-    # the result of raising the base (RDI) to the power of the exponent (RSI).
-    # no local variables
-    enter $0, $0
+    # RDI = BASE
+    # RSI = EXPONENT
+    # returns BASE^EXPONENT in RAX
+
+    # local variables.
+    # -8(%rbp) = counter (for loop) initialized to EXPONENT
+    enter $16, $0
 
     # base to the power of 0 is 1
     mov $1, %rax
+
     # number of times to multiply base by itself
-    mov %rsi, %rcx
-loop:
-    mul %rdi
-    loop loop
+    mov %rsi, -8(%rbp)
+
+mainloop:
+    # multiply base by itself
+    mulq %rdi
+    
+    # decrement counter
+    decq -8(%rbp)
+    jnz mainloop
 
     # return value already in rax
     leave
