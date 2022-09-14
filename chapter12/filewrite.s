747af97cd79df8a212ecef610916f60673ea38a9
chris-miner <94078897+chris-miner@users.noreply.github.com>
2022-09-13 19:14:56 -0600

    fixed bad access error. missing leave at end of function. Added Josh's info.

--- chapter12/filewrite.s
+++ chapter12/filewrite.s
@@ -3,23 +3,24 @@
 .data
 filename:
     .ascii "my-out.txt\0"
-    
 mode:
     .ascii "w\0"
 
 format1:
     .ascii "The age of %s is %d.\n\0"
-
 sallyname:
-    .ascii "Sally\n\0"
-
+    .ascii "Sally\0"
 sallyage:
     .quad 53
 
 format2:
     .ascii "%d and %d are %s's favorite numbers.\n\0"
-
-
+joshname:
+    .ascii "Josh\0"
+joshfavorite1:
+    .quad 7
+joshfavorite2:
+    .quad 13
 
 .text
 .globl _main
@@ -31,6 +32,8 @@ _main:
     lea filename(%rip), %rdi
     lea mode(%rip), %rsi
     call _fopen
+
+    # save the file pointer in a local variable
     mov %rax, FILE(%rbp)
 
     # write the first formatted string to the file
@@ -38,12 +41,25 @@ _main:
     lea format1(%rip), %rsi
     lea sallyname(%rip), %rdx
     mov sallyage(%rip), %rcx
+    # not passing floating point values to the function
+    mov $0, %rax
     call _fprintf
 
     # write the second formatted string to the file
+    mov FILE(%rbp), %rdi
+    lea format2(%rip), %rsi
+    mov joshfavorite1(%rip), %rdx
+    mov joshfavorite2(%rip), %rcx
+    lea joshname(%rip), %r8
+
+    # not passing floating point values to the function
+    mov $0, %rax
+    call _fprintf
 
     # close the file
     mov FILE(%rbp), %rdi
     call _fclose
     # exit the program
+    mov $0, %rax
+    leave
     ret
