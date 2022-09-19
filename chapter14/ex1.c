be0944d94d2e945b4ecca29e9d5ed47206992ee6
chris-miner <94078897+chris-miner@users.noreply.github.com>
2022-09-19 11:08:19 -0600

    program to allocate memory, print address, initialize memory, and print it.

---
+++ chapter14/ex1.c
@@ -0,0 +1,27 @@
+/*
+    chapter 14 exercise 1
+    Write a program that takes a number of bytes from the user,
+    allocates that amount of memory, and fills it with the letter 'a'.
+    Then it should print the memory address of the allocated memory,
+    and the contents of the memory.
+*/
+#include <stdio.h>
+
+void *allocate(int);
+void deallocate(void *);
+
+int main()
+{
+    int bytes;
+    fprintf(stdout, "Enter number of bytes to allocate: ");
+    fscanf(stdin, "%d", &bytes);
+
+    char *a = allocate(bytes);
+    for (int i = 0; i < bytes; i++)
+        a[i] = 'a';
+
+    fprintf(stdout, "Allocated %d bytes at address %p\n", bytes, a);
+    fprintf(stdout, "Contents: %s\n", a);
+    deallocate(a);
+    return 0;
+}
